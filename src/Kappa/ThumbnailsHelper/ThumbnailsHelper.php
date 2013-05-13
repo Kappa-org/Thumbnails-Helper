cf85365bacf9d0d9226f0a4074c8822bcd3cca4d
Budry <zarubaondra@gmail.com>
2013-05-13 12:49:45 +0200

    Refactored

--- src/Kappa/ThumbnailsHelper/ThumbnailsHelper.php
+++ src/Kappa/ThumbnailsHelper/ThumbnailsHelper.php
@@ -1,168 +1,113 @@
 <?php
 /**
  * ThumbnailsHelper.php
- * Autgor: Ondřej Záruba <zarubaondra@gmail.com>
- * Date: 20.12.12
+ *
+ * @author Ondřej Záruba <zarubaondra@gmail.com>
+ * @date 20.12.12
+ *
+ * @package Kappa\ThumbnailsHelper
  */
 
 namespace Kappa\ThumbnailsHelper;
 
-use Kappa\Utils\Strings;
-use Kappa\Utils\Validators;
-use Kappa\Utils\FileSystem\Files;
-use Kappa\Utils\FileSystem\Directories;
-use Nette\Image;
+use Kappa\FileSystem\Directory;
+use Kappa\FileSystem\File;
+use Kappa\FileSystem\Image;
 use Nette\Object;
 
+/**
+ * Class ThumbnailsHelper
+ *
+ * @package Kappa\ThumbnailsHelper
+ */
 class ThumbnailsHelper extends Object
 {
 	/** @var array */
 	private $params;
 
-	/** @var array */
-	private $sizes;
-
-	/** @var string */
-	private $flags;
-
-	/** @var string */
-	private $originalImage;
-
-	/** @var string */
-	private $thumbImage;
-
 	/**
 	 * @param string $wwwDir
 	 * @param string $thumbDir
 	 * @param null|int $frequencyControl
-	 * @throws \Kappa\DirectoryNotFoundException
-	 * @throws \Kappa\InvalidArgumentException
+	 * @throws DirectoryNotFoundException
 	 */
 	public function __construct($wwwDir, $thumbDir, $frequencyControl = null)
 	{
 		if (!is_dir($wwwDir)) {
-			throw new \Kappa\DirectoryNotFoundException(__METHOD__, $wwwDir);
+			throw new DirectoryNotFoundException("Directory '$wwwDir' has not been found");
 		}
-		if (!is_int($frequencyControl) && $frequencyControl !== null) {
-			throw new \Kappa\InvalidArgumentException("Class " . __METHOD__ . " required as third argument integer");
+		if (!is_dir($thumbDir)) {
+			throw new DirectoryNotFoundException("Directory '$thumbDir' has not been found");
 		}
-		$this->params['wwwDir'] = realpath($wwwDir);
-		$this->params['thumbDir'] = $this->checkThumbDir($thumbDir);
-		$this->params['frequencyControl'] = $frequencyControl;
+		$this->params = array(
+			'wwwDir' => realpath($wwwDir),
+			'thumbDir' => realpath($thumbDir),
+		);
+		if ($frequencyControl) {
+			$this->checkThumbs((int)$frequencyControl);
+		}
 	}
 
 	/**
-	 * @param string $src
+	 * @param string $original
 	 * @param array $sizes
 	 * @param string $flag
 	 * @return string
-	 * @throws \Kappa\InvalidArgumentException
-	 * @throws \Kappa\FileNotFoundException
-	 * @throws \Kappa\ImageNotFoundException
 	 */
-	public function thumb($src, array $sizes = array(300, 200), $flag = "FIT")
+	public function process($original, array $sizes = array(300, 100), $flag = "fit")
 	{
-		$this->sizes = $sizes;
-		if (defined('\Nette\Image::' . $flag)) {
-			$this->flags = constant('\Nette\Image::' . $flag);
+		$original = new File($this->params['wwwDir'] . DIRECTORY_SEPARATOR . $original);
+		$thumb = $this->createThumbName($original, $sizes);
+		$imageInfo = @getimagesize($original->getInfo()->getPathname());
+		if (file_exists($thumb) || ($imageInfo[0] <= $sizes[0] && $imageInfo[1] <= $sizes[1])) {
+			$file = new File($thumb);
+			return $file->getInfo()->getRelativePath($this->params['wwwDir']);
 		} else {
-			throw new \Kappa\InvalidArgumentException("Flag '$flag' not found. Please select only between FIT, FILL, EXACT, SHRINK_ONLY, STRETCH flags");
-		}
-		$this->prepare($src);
-		if (!file_exists($this->originalImage)) {
-			throw new \Kappa\FileNotFoundException(__METHOD__, $src);
-		}
-		if (!Validators::isImage($this->originalImage)) {
-			throw new \Kappa\ImageNotFoundException(__METHOD__, $src);
-		}
-		if (file_exists($this->thumbImage)) {
-			return $this->getRelativePath($this->thumbImage);
-		} else {
-			$imgInfo = getimagesize($this->originalImage);
-			if ($imgInfo[0] <= $this->sizes[0] || $imgInfo[1] <= $this->sizes[1]) {
-				return $this->getRelativePath($this->originalImage);
-			} else {
-				$this->createThumb();
-				return $this->getRelativePath($this->thumbImage);
-			}
+			$file = new Image($original->getInfo()->getPathname(), $thumb, $sizes, $flag);
+			return $file->getInfo()->getRelativePath($this->params['wwwDir']);
 		}
 	}
 
 	/**
-	 * @param string $relativePath
+	 * @param File $original
+	 * @param array $sizes
+	 * @return mixed
 	 */
-	private function prepare($relativePath)
+	private function createThumbName(File $original, array $sizes)
 	{
-		$this->originalImage = Strings::repairPathSeparators($this->params['wwwDir'] . $relativePath);
-		$this->thumbImage = $this->createThumbName();
-		if ($this->params['frequencyControl'] !== null) {
-			$this->deleteOlderThumbnails();
-		}
-	}
 
-	/**
-	 * @return string
-	 */
-	private function createThumbName()
-	{
 		$path = $this->params['thumbDir'];
-		$path .= (string)strrchr($this->originalImage, DIRECTORY_SEPARATOR);
-		$type = (string)strrchr($this->originalImage, ".");
+		$path .= '/' . $original->getInfo()->getBasename();
 
 		$newName = "_thumb";
-		$newName .= $this->sizes[0] . 'x' . $this->sizes[1];
-		$newName .= '_' . Files::md5sum($this->originalImage);
-		$newName .= '_' . fileatime($this->originalImage);
-		$newName .= $type;
+		$newName .= $sizes[0] . 'x' . $sizes[1];
+		$newName .= '_' . $original->getHash();
+		$newName .= '_' . $original->getInfo()->getMTime();
+		$newName .= $original->getInfo()->getFileExtension();
 
-		$thumbName = str_replace($type, $newName, $path);
+		$thumbName = str_replace($original->getInfo()->getFileExtension(), $newName, $path);
+
 		return $thumbName;
 	}
 
 	/**
-	 * @param string $path
-	 * @return string
+	 * @param int $day
 	 */
-	private function getRelativePath($path)
+	private function checkThumbs($day)
 	{
-		$to = strlen($this->params['wwwDir']);
-		$relative = substr($path, $to);
-		return $relative;
-	}
-
-	private function createThumb()
-	{
-		list($width, $height) = $this->sizes;
-		$image = Image::fromFile($this->originalImage);
-		$image->resize($width, $height, $this->flags);
-		$image->save($this->thumbImage);
-	}
-
-	private function checkThumbDir($thumbDir)
-	{
-		$dir = $this->params['wwwDir'];
-		$dir .= DIRECTORY_SEPARATOR;
-		$dir .= $thumbDir;
-		if (!is_dir($dir)) {
-			Directories::create($dir, '0777');
-		}
-		return realpath($dir);
-	}
-
-	private function deleteOlderThumbnails()
-	{
-		$path = $this->params['thumbDir'];
-		$day = $this->params['frequencyControl'];
-		$file = $path . '/check.txt';
-		if (file_exists($file)) {
-			$lastControl = Files::read($file);
-			if ($lastControl <= (strtotime("now") - (60 * 60 * 24 * $day))) {
-				$filesForDelete = Files::selectOlderThan($path, $day);
-				Files::deleteFiles($filesForDelete);
+		$controlFile = new File($this->params['thumbDir'] . '/lastControl.txt');
+		$lastControl = (int)$controlFile->read();
+		$time = strtotime("now") - (60 * 60 * 24 * $day);
+		if ($lastControl <= $time) {
+			$directory = new Directory($this->params['thumbDir']);
+			$files = $directory->getFiles();
+			foreach ($files as $path => $file) {
+				if ($file->getMTime() <= $time) {
+					$_file = new File($path);
+					$_file->remove();
+				}
 			}
-			Files::rewrite($file, strtotime("now"));
-		} else {
-			Files::create($file, strtotime("now"));
 		}
+		$controlFile->overwrite(strtotime('now'));
 	}
 }
