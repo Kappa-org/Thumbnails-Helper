4171a74798a21135e83600b303cd776034020003
Budry <zarubaondra@gmail.com>
2013-05-13 12:49:07 +0200

    Added exceptions

---
+++ src/Kappa/ThumbnailsHelper/exceptions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * exceptions.php
+ *
+ * @author Ondřej Záruba <zarubaondra@gmail.com>
+ * @date 13.5.13
+ *
+ * @package Kappa\ThumbnailsHelper
+ */
+
+namespace Kappa\ThumbnailsHelper;
+
+/**
+ * Class DirectoryNotFoundException
+ *
+ * @package Kappa\ThumbnailsHelper
+ */
+class DirectoryNotFoundException extends \LogicException
+{
+
+}
+
+/**
+ * Class InvalidArgumentException
+ *
+ * @package Kappa\ThumbnailsHelper
+ */
+class InvalidArgumentException extends \LogicException
+{
+
+}
