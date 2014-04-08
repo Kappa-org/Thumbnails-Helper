24b0cea19d5948648ea75957c0b01a4cac85e2be
Budry <zarubaondra@gmail.com>
2014-04-08 13:32:23 +0200

    Added DirectoryNotFound and IvalidArgument exceptions

---
+++ src/Kappa/ThumbnailsHelper/exceptions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the Kappa\ThumbnailsHelper package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\ThumbnailsHelper;
+
+/**
+ * Class DirectoryNotFoundException
+ * @package Kappa\ThumbnailsHelper
+ */
+class DirectoryNotFoundException extends \LogicException
+{
+
+}
+
+/**
+ * Class InvalidArgumentException
+ * @package Kappa\ThumbnailsHelper
+ */
+class InvalidArgumentException extends \LogicException
+{
+
+}
